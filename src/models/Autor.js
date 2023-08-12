abaddae1adfe7e7808a97c5654548eb7496e93cc
João Paulo <joaowais@gmail.com>
2023-08-12 19:31:51 -0300

    Aula 05 Finalizada

---
+++ src/models/Autor.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const autorEschema = new mongoose.Schema(
+  {
+    id: { type: String },
+    nome: { type: String, required: true },
+    nacionalidade: { type: String}
+  },
+  {
+    versionKey: false
+  }
+)
+
+const autores = mongoose.model('autores', autorEschema);
+
+export default autores;
